cd13ff6a6b0fab7fdf99fb3d98fa1bb48494d630
kaarthikeyan <kaarthikeyan0909@gmail.com>
2024-09-27 09:11:38 +0530

    Second comiit

--- app.py
+++ app.py
@@ -3,25 +3,28 @@ from langchain import PromptTemplate, LLMChain
 from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
 
-# Import the Local Environment
-from dotenv import load_dotenv
+# Import the local environment
+from dotenv import load_dotenv # for api key storing
 import os
 load_dotenv()
-genai.configure(api_key =os.getenv('GOOGLE-API-KEY'))
+genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
+
+
 # Design the Page ... streamlit run app.py
-headers = {'authorization':st.secrets['GOOGLE-API-KEY'],"content-type":"application/json"}
+header = {'authorization':st.secrets["GOOGLE-API-KEY"],
+          "content-type":"application/json"}
 st.title("Movie Recommender Systems")
 user_input = st.text_input("Enter the Movie Title,genre or keyword")
 
 # creating a prompt Template
-demo_template = '''I want you to act as a Dietician and help me with {prompt}'''
+demo_template = ''' Give me movie recommendations for the following genre {prompt}'''
 template = PromptTemplate(
     input_variables = ['prompt'],
     template = demo_template)
 
 
 # Google Gemini Model
-llm = ChatGoogleGenerativeAI(model="gemini-pro",api_key='GOOGLE-API-KEY')
+llm = ChatGoogleGenerativeAI(model="gemini-pro",api_key=os.getenv("GOOGLE-API-KEY"))
 
 
 if user_input:
